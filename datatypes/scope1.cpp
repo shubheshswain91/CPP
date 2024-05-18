db7218078c81736339e73bfde07b984f63d70484
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-18 18:11:55 +0200

    Local Scope

--- datatypes/scope1.cpp
+++ datatypes/scope1.cpp
@@ -8,7 +8,7 @@ void func()
 	// this variable is local to the 
 	// function func() and cannot be 
 	// accessed outside this function 
-	int age=18;	 
+	int age=18;	  // only valid for this function
 } 
 
 int main() 
